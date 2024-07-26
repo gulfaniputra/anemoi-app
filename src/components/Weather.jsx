f08006d5de5c9121fffac97f1c464cc3dbf47053
Gulfani Putra <gulfaniputra@gmail.com>
2024-07-26 12:08:06 +0800

    Add a search bar to the UI

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,21 @@
 import './Weather.css';
+import search_icon from '../assets/search.png';
 
 const Weather = () => {
-  return <div className="weather"></div>;
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search"
+        />
+        <img
+          src={search_icon}
+          alt="The search box's icon."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
