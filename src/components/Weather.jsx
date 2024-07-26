607c406d35841bb1990d4fde8c349a1554752e90
Gulfani Putra <gulfaniputra@gmail.com>
2024-07-26 14:18:46 +0800

    Setup weather icons, temperature, & location

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,12 @@
 import './Weather.css';
 import search_icon from '../assets/search.png';
+import clear_icon from '../assets/clear.png';
+import cloud_icon from '../assets/cloud.png';
+import drizzle_icon from '../assets/drizzle.png';
+import rain_icon from '../assets/rain.png';
+import snow_icon from '../assets/snow.png';
+import wind_icon from '../assets/wind.png';
+import humidity_icon from '../assets/humidity.png';
 
 const Weather = () => {
   return (
@@ -14,6 +21,13 @@ const Weather = () => {
           alt="The search box's icon."
         />
       </div>
+      <img
+        src={clear_icon}
+        alt="A weather icon."
+        className="weather-icon"
+      />
+      <p className="temperature">16°C</p>
+      <p className="location">Birmingham</p>
     </div>
   );
 };
