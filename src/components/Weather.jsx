29e1246f981da351251fe2866fb16291ee83f233
Gulfani Putra <gulfaniputra@gmail.com>
2024-07-26 16:22:56 +0800

    Add API data with 'weatherData' state variable

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -7,22 +7,52 @@ import rain_icon from '../assets/rain.png';
 import snow_icon from '../assets/snow.png';
 import wind_icon from '../assets/wind.png';
 import humidity_icon from '../assets/humidity.png';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Weather = () => {
+  const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    '01d': clear_icon,
+    '01n': clear_icon,
+    '02d': cloud_icon,
+    '02n': cloud_icon,
+    '03d': cloud_icon,
+    '03n': cloud_icon,
+    '04d': drizzle_icon,
+    '04n': drizzle_icon,
+    '09d': rain_icon,
+    '09n': rain_icon,
+    '10d': rain_icon,
+    '10n': rain_icon,
+    '13d': snow_icon,
+    '13n': snow_icon,
+  };
+
   const search = async city => {
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
         import.meta.env.VITE_APP_ID
       }`;
       const response = await fetch(url);
       const data = await response.json();
+
       console.log(data);
+
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: Math.floor(data.wind.speed),
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
     } catch (error) {}
   };
 
   useEffect(() => {
-    search('Birmingham');
+    search('Rome');
   }, []);
 
   return (
@@ -42,8 +72,8 @@ const Weather = () => {
         alt="A weather icon."
         className="weather-icon"
       />
-      <p className="temperature">21&#8451;</p>
-      <p className="location">Birmingham</p>
+      <p className="temperature">{weatherData.temperature}&deg;C</p>
+      <p className="location">{weatherData.location}</p>
       <div className="weather-data">
         <div className="col">
           <img
@@ -51,7 +81,7 @@ const Weather = () => {
             alt="The humidity icon"
           />
           <div>
-            <p>87%</p>
+            <p>{weatherData.humidity}&#x00025;</p>
             <span>Humidity</span>
           </div>
         </div>
@@ -61,7 +91,7 @@ const Weather = () => {
             alt="The wind speed icon"
           />
           <div>
-            <p>11.2 km/h</p>
+            <p>{weatherData.windSpeed}km/h</p>
             <span>Wind Speed</span>
           </div>
         </div>
